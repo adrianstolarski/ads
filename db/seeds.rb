cf7d8351f95427bea1ec2cc487ac64eebfb74998
adrianstolarski <adrian.stolarski@nopio.com>
2015-05-30 19:10:37 +0200

    commit all files

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+ ads = Ad.create(name: 'Kon', description: 'kon jaki jest kazdy widzi', price: '123.00', seller_id: '1', email: '123@wp.pl')
